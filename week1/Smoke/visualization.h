ff5b93e27ab395301f31851d5eefb16a4811efac
Maarten Terpstra <m.l.terpstra@student.rug.nl>
2014-11-12 12:39:06 +0100

    Removed more comments, and added some more

--- week1/Smoke/visualization.h
+++ week1/Smoke/visualization.h
@@ -16,10 +16,9 @@ private:
     int frozen;                 //toggles on/off the animation
     float vec_scale;            //scaling of hedgehogs
 
-public:
-    //Visualization(int a_color_dir, float a_vec_scale, int a_scalar_col, int a_frozen) : color_dir(a_color_dir), vec_scale(a_vec_scale), COLOR_BLACKWHITE(0), COLOR_RAINBOW(1), COLOR_BANDS(2), scalar_col(a_scalar_col), frozen(a_frozen){}
-    Visualization(int a_color_dir, int a_scalar_col, int a_frozen, float a_vec_scale) : color_dir(a_color_dir), COLOR_BLACKWHITE(0), COLOR_RAINBOW(1), COLOR_BANDS(2), scalar_col(a_scalar_col), frozen(a_frozen), vec_scale(a_vec_scale){}
+public:    
     //------ VISUALIZATION CODE STARTS HERE -----------------------------------------------------------------
+    Visualization(int a_color_dir, int a_scalar_col, int a_frozen, float a_vec_scale) : color_dir(a_color_dir), COLOR_BLACKWHITE(0), COLOR_RAINBOW(1), COLOR_BANDS(2), scalar_col(a_scalar_col), frozen(a_frozen), vec_scale(a_vec_scale){}
     //rainbow: Implements a color palette, mapping the scalar 'value' to a rainbow color RGB
     void rainbow(float value, float* R, float* G, float* B);
 
@@ -37,18 +36,22 @@ public:
     //                    using a rainbow colormap. If method==0, simply use the white color
     void direction_to_color(float x, float y, int method);
 
+    // Returns true when animation is paused, false otherwise
     int isFrozen(){
         return frozen;
     }
 
+    // Sets status to frozen if animation is enabled and vice versa
     void toggleFrozen(){
         frozen = 1 - frozen;
     }
 
+    //Sets hedgehog color to black and white if color is used and vice versa
     void toggleDirectionColor(){
         color_dir = 1 - color_dir;
     }
 
+    //Select next color profile
     void nextColor(){
         scalar_col++;
         if (scalar_col > COLOR_BANDS)
@@ -57,6 +60,8 @@ public:
         }
     }
 
+    // Scale the hedgehog length by a percentage
+    // So putting scale = 1.2 makes the hedgehogs 20% larger
     void scaleHedgehogLength(float scale){
         vec_scale *= scale;
     }
