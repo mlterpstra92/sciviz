21254b6164934ea9abb0ea8204d7b7aca534630d
Maarten Terpstra <m.l.terpstra@student.rug.nl>
2014-11-19 11:33:01 +0100

    Reorganized some code

--- week1/Smoke/fluids.h
+++ week1/Smoke/fluids.h
@@ -3,10 +3,5 @@
 int direction_coloring, drawMatter, drawHedgehogs, scalarColoring, animate;
 float timeStep, hedgehogScale, viscosityScale;
 enum {ANIMATE_ID, SCALAR_COLORING_ID, DRAW_HEDGEHOGS_ID, DRAW_MATTER_ID, DIRECTION_COLOR_ID, NEXT_COLOR_ID, TIMESTEP_SPINNER_ID, HEDGEHOG_SPINNER_ID, VISCOSITY_SPINNER_ID};
-/*int ANIMATE_ID=0;
-int SCALAR_COLORING_ID=1;
-int DRAW_HEDGEHOGS_ID=2;
-int DRAW_MATTER_ID=3;
-int DIRECTION_COLOR_ID=4;*/
 void visualize();
 #endif
